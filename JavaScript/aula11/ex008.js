c2277b6e2fdaa0696a38c4a1a0f300235cf5de16
Rasyon <MISAELRH@PROTON.ME>
2025-06-26 17:06:38 -0300

    Add files via upload

---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,8 @@
+var velo = parseFloat(prompt(`Registre a velocidade do carro: `));
+
+if (velo > 60) {
+    console.log(`A velocidade do seu carro é ${velo}Km/h, e é acima do limite máximo que é 60! Então você será MULTADO!  `)
+} else {
+    console.log(`A velocidade do seu carro é ${velo}. Boa rapaz, está andando na velocidade permitida, continue dirigindo com cuidado!`)
+}
+    console.log(`Dirija sempre com cinto de segurança mano!`)
