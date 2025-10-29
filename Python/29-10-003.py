a38433fc34b0a515e18461e0fd9b6b73e3e09f15
Rasyon <MISAELRH@PROTON.ME>
2025-10-29 20:59:13 -0300

    Add files via upload

---
+++ Python/29-10-003.py
@@ -0,0 +1,21 @@
+class Retangulo:
+    def __init__(self, comprimento, largura):
+        self.comprimento = comprimento
+        self.largura = largura
+    
+    def mudarLados(self, novoComprimento, novaLargura):
+        self.comprimento = self.novoComprimento 
+        self.largura = self.novaLargura
+        print('Tamanho atualizado!')
+    def retornarLados(self):
+        return (self.comprimento, self.largura)
+    def calcArea(self):
+        return self.comprimento*self.largura
+    def calcPerimetro(self):
+        return (self.comprimento*2)+(self.largura*2)
+    
+def calcConstrucao():
+    
+
+
+    
